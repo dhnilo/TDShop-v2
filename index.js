564d29b3f9d8b623d94a287e1b8137103dae903a
Thaddeus-Bielecki <twb5147@truman.edu>
2023-12-02 15:36:44 -0600

    Frontend overhaul of css
    made unique css files for each screen

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('dropdown-button').addEventListener('click', function(event) {
-  document.getElementById('dropdown-content').classList.toggle('show');
-  event.stopPropagation();
-});
-
-document.addEventListener('click', function() {
-  document.getElementById('dropdown-content').classList.remove('show');
-});
