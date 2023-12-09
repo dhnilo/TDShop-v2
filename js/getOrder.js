604abead349eab9b1007e619d67548d1104fb42a
Drew Hnilo <dhnilo.dev@gmail.com>
2023-12-09 16:48:25 -0600

    Completed order screen functionality

--- js/getOrder.js
+++ js/getOrder.js
@@ -1,7 +1,7 @@
 function getOrder() {
   // Get the orderID from the URL
   const urlParams = new URLSearchParams(window.location.search);
-  const orderID = urlParams.get('orderID');
+  const orderID = urlParams.get("orderID");
 
   // Select the container
   const container = document.querySelector("#orderContainer");
@@ -12,47 +12,83 @@ function getOrder() {
     .then((data) => {
       // Handle the response data
 
-      // Create HTML elements and set their content to the order data
-      const orderIdElement = document.createElement("p");
-      orderIdElement.textContent = `Order ID: ${data.orderID}`;
-
-      const userIdElement = document.createElement("p");
-      userIdElement.textContent = `User ID: ${data.userId}`;
-
-      // Append the new elements to the container
-      container.appendChild(orderIdElement);
-      container.appendChild(userIdElement);
+      // Display the order ID
+      const titleElement = document.querySelector(".title");
+      titleElement.textContent += `Your Order ID: ${data.orderID}`;
 
       // Parse the cart and shippingInfo JSON strings back into objects
-      if (typeof data.cart === 'string') {
+      if (typeof data.cart === "string") {
         data.cart = JSON.parse(JSON.parse(data.cart));
       }
-      if (typeof data.shippingInfo === 'string') {
+      if (typeof data.shippingInfo === "string") {
         data.shippingInfo = JSON.parse(JSON.parse(data.shippingInfo));
       }
 
-      // Loop through the cart array
-      data.cart.forEach((item) => {
-        // Create HTML elements for each item
-        const itemElement = document.createElement("p");
-        itemElement.textContent = `Item: ${item.name}, Quantity: ${item.qty}`;
-
-        // Append the new element to the container
-        container.appendChild(itemElement);
-      });
+      var totalItemPrice = 0;
+      var shippingPrice = 10;
 
       // Display the order items
-      const orderTable = document.querySelector('#orderTable tbody');
-      data.cart.forEach(item => {
-          const row = document.createElement('tr');
-          row.innerHTML = `
+      const orderTable = document.querySelector("#orderTable tbody");
+      data.cart.forEach((item) => {
+        totalItemPrice += item.price * item.qty;
+        const row = document.createElement("tr");
+        row.innerHTML = `
+              <td><img src="${item.image}" width="100px"></td>s  
               <td>${item.name}</td>
-              <td>${item.price}</td>
+              <td>$${item.price}</td>
               <td>${item.qty}</td>
           `;
-          orderTable.appendChild(row);
+        orderTable.appendChild(row);
       });
 
       console.log("Order data:", data);
+
+      // Display the shipping info
+      const shippingElement = document.querySelector("#shipping");
+      shippingElement.textContent = `Shipping: ${data.shippingInfo.address}, ${data.shippingInfo.city}, ${data.shippingInfo.postalCode}, ${data.shippingInfo.country}`;
+
+      // Display the item subtotal
+      const itemSubtotalElement = document.querySelector("#itemSubtotal");
+      itemSubtotalElement.textContent = `Item Subtotal: $${totalItemPrice.toFixed(2)}`;
+
+      // Calculate and display the tax
+      const taxRate = 0.1; // Update this to your actual tax rate
+      const taxPrice = totalItemPrice * taxRate;
+      const itemTaxElement = document.querySelector("#itemTax");
+      itemTaxElement.textContent = `Tax: $${taxPrice.toFixed(2)}`;
+
+      // Display the shipping price in the order summary
+      const shippingPriceElement = document.querySelector("#shippingPrice");
+      if (totalItemPrice > 100) {
+        shippingPrice = 0;
+      } else {
+        shippingPrice = 10;
+      }
+      shippingPriceElement.textContent = `Shipping Price: $${shippingPrice.toFixed(2)}`;
+
+      // Calculate and display the total
+      const totalPrice = totalItemPrice + taxPrice + shippingPrice;
+      const totalElement = document.querySelector("#total");
+      totalElement.textContent = `Total: $${totalPrice.toFixed(2)}`;
+
+      paypal.Buttons({
+        createOrder: function (data, actions) {
+            // This function sets up the details of the transaction, including the amount and line item details.
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: totalPrice.toFixed(2)
+                    }
+                }]
+            });
+        },
+        onApprove: function (data, actions) {
+            // This function captures the funds from the transaction.
+            return actions.order.capture().then(function (details) {
+                // This function shows a transaction success message to your buyer.
+                alert('Transaction completed by ' + details.payer.name.given_name);
+            });
+        }
+    }).render('#paypal-button-container'); // Replace '#paypal-button-container' with the ID of the div where you want the PayPal buttons to appear
     });
 }
